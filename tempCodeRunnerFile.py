1a9bcfee02263d82e4acabfecd17121ca7f06e28
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-10-26 16:14:04 +0300

    converted javascript into python

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,7 +3,7 @@ rows = int(input("Enter the number of rows"))
 for i in range(rows):
   row = ""
   number = 1
-  for j in range(i + 1):
+  for j in range(i):
     row += str(number) + " "
     number += 2
   print(row)
